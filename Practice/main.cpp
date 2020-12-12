049292078ba47b7a07bcf8692c625e3a5ab5e09c
Jason24-Zeng <z5zeng@eng.ucsd.edu>
2020-12-12 11:25:42 +0800

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Zijian Zeng on 2020/12/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
